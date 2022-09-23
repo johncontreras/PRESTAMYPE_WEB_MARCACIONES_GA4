d1ea401ec0267dfa03d302cfe0ff60d7aaeefbb0
John Alex Contreras Soto <johnalex.cs@gmail.com>
2022-09-23 18:40:22 -0500

    Registro_step0_signUp
    Registro exitoso.

---
+++ auth_registro_step0_signUp.js
@@ -0,0 +1,22 @@
+//Ruta: /registro
+//Disparador: Al enviarse correctamente el formulario de Registro.
+
+window.dataLayer = window.dataLayer || [];
+window.dataLayer.push({
+  event: "registro.step0_signUp",
+  //PRODUCTO:
+  producto: "inversion", // Producto desde cuyo cotizador se inicia el registro.
+  producto_tipo: "prestamos", // Tipo de producto o variante seleccionada previamente en el cotizador
+
+  //PERFIL
+  operacion_perfil: "Empresario", // 0 "Inversionista", encaso esté disponible esta información.
+
+  //USUARIO:
+  user_email: "johnalex.cs@gmail.com", //email ingresado en el formulario.
+  user_tipoDocumento: "DNI",
+  user_numDocumento: "43582572",
+  user_id: "628c18cc9de1a9674ab8fb20", // En caso este disponible. Id único generado a partir de su DNI.
+  user_telefono1: "946346772",
+  user_tipo: "registrado", //registrado: para usuarios registrados en plataforma. "no registrado": para usuarios no registrados.
+  visita_tipo: "logueado", //logueado: para usuarios son sesion inicida en plataforma. "no logueado": para usuarios sin sesion iniciada.
+});
