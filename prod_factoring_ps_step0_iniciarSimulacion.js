d7f7d3c3db114ec4b93454ce29da0de649a7700a
John Alex Contreras Soto <johnalex.cs@gmail.com>
2022-09-23 19:18:45 -0500

    Factoring - Zona Publica - step0_iniciarSimulacion
    Click en el botón "Simula tu adelanto"

---
+++ prod_factoring_ps_step0_iniciarSimulacion.js
@@ -0,0 +1,24 @@
+//Producto: Factoring
+//Perfil: Empresario
+//Ruta: /factoring
+//Disparador: Click en el botón "Simula tu adelanto"
+
+window.dataLayer = window.dataLayer || [];
+window.dataLayer.push({
+  event: "factoring.step0_iniciarSimulacion",
+  //PRODUCTO:
+  producto: "factoring",
+
+  //PERFIL
+  operacion_perfil: "Empresario", // 0 "Inversionista", encaso esté disponible esta información.
+
+  //USUARIO:
+  user_id: "628c18cc9de1a9674ab8fb20", // En caso este disponible. Id único generado a partir de su DNI.
+  user_tipo: "registrado", //registrado: para usuarios registrados en plataforma. "no registrado": para usuarios no registrados.
+  visita_tipo: "logueado", //logueado: para usuarios son sesion inicida en plataforma. "no logueado": para usuarios sin sesion iniciada.
+  operacion_perfil: "Empresario", // 0 "Inversionista", encaso esté disponible esta información.
+});
+
+
+
+
