574f2dbcc092e9757b3cfa14c46e5570f7f84a31
John Alex Contreras Soto <johnalex.cs@gmail.com>
2022-09-23 19:34:15 -0500

    Inversion Factoring - Zona Publica - step0_iniciarSimulacion
    Click en el botón "Registrate"

---
+++ prod_inversionFactoring_ps_step0_iniciarSimulacion.js
@@ -0,0 +1,20 @@
+//Producto: Inversion
+//Ruta: /invertir-factoring
+//Disparador: Click en el botón "Registrate"
+
+window.dataLayer = window.dataLayer || [];
+window.dataLayer.push({
+  event: "inversionFactoring.step0_iniciarSimulacion",
+  
+  //PRODUCTO:
+  producto: "inversion", // Producto desde cuyo cotizador se inicia el login.
+  producto_tipo: "factoring", // Tipo de producto o variante seleccionada previamente en el cotizador
+
+  //PERFIL:
+  operacion_perfil: "Inversionista", // 0 "Inversionista", encaso esté disponible esta información.
+
+  //USUARIO:
+  user_id: "628c18cc9de1a9674ab8fb20", //En caso este disponible. Id único generado a partir de su DNI.
+  user_tipo: "registrado", //registrado: para usuarios registrados en plataforma. "no registrado": para usuarios no registrados.
+  visita_tipo: "logueado" //logueado: para usuarios son sesion inicida en plataforma. "no logueado": para usuarios sin sesion iniciada.
+});
