fb88657e94766ae5dbd876f3a44ddd80c7bd31ac
John Alex Contreras Soto <johnalex.cs@gmail.com>
2022-09-23 19:51:00 -0500

    Inversion Factoring - Zona Privada - step3_confirmacionInversion.js

---
+++ prod_inversionFactoring_zp_step3_confirmacionInversion.js
@@ -0,0 +1,35 @@
+//Producto: Inversion
+//Ruta: /app/inversionista/oportunidades
+//Disparador: Click en el botón "Realizar Inversion" luego de ingresar el monto a invertir.
+
+window.dataLayer = window.dataLayer || [];
+window.dataLayer.push({
+  event: "inversionFactoring.step3_confirmacionInversion",
+  //PRODUCTO:
+  producto: "inversion", // Producto desde cuyo cotizador se inicia el login.
+  producto_tipo: "factoring", // Tipo de producto o variante seleccionada previamente en el cotizador
+
+  //USUARIO:
+  user_id: "628c18cc9de1a9674ab8fb20", //En caso este disponible. Id único generado a partir de su DNI.
+  user_tipo: "registrado", //registrado: para usuarios registrados en plataforma. "no registrado": para usuarios no registrados.
+  visita_tipo: "logueado", //logueado: para usuarios son sesion inicida en plataforma. "no logueado": para usuarios sin sesion iniciada.
+
+  //PERFIL
+  operacion_perfil: "Inversionista", // 0 "Inversionista", encaso esté disponible esta información.
+
+  //Oportunidad:
+  subasta_codigo: "l7yz6pqt",
+  subasta_modelo: "velocidad", // String: Modelo seleccionado de la subasta.
+  subasta_moneda: "PEN",
+  riesgo: "C", //String. Riesgo asignado a la subasta.
+  rendimiento: "1.35", //String. Rendimiento/TIR. Máximo dos decimales.(1.35% = 1.35)
+  monto_financiar: "20000.00", //String. Monto a financiar.
+  pago_fecha: "27/07/2022", // String. Fecha de pago.
+  pago_diasAnticipacion: "31", //Entero. Número de días restantes hasta la fecha de pago.
+
+  //Inversion
+  inversion_monto: "9000.00", //String. Monto ingresao a invertir. Maximo dos decimales.
+  inversion_moneda: "PEN",
+  inversion_id: "xyz123", // String. Id de la transacción.
+  isCompraTotal: "true", //Booleano. Si el usuario marcó el checkbox de Compra total.
+});
