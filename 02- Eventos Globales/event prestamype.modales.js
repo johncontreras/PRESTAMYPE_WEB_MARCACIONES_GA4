83f10fb1d6e13f95e0c44626282105b50b675d82
John Alex Contreras Soto <johnalex.cs@gmail.com>
2022-10-09 19:54:27 -0500

    Update 02- Eventos Globales/event prestamype.modales.js

--- 02- Eventos Globales/event prestamype.modales.js
+++ 02- Eventos Globales/event prestamype.modales.js
@@ -6,9 +6,13 @@
 
 window.dataLayer = window.dataLayer || [];
 window.dataLayer.push({
-  event: "prestamype.modales",
-  modal_categoria: "Datos Faltantes", //posibles valores: Informativo, Datos Faltantes, Datos Incorrectos, Alerta
-
-  modal_mensajeUsuario: "Tú cámara no está activa" // Mensaje principal del modal.  En caso incluya algún dato del usuario, como su nombre, debe ser reemplazado por un texto genérico. Ejemplo1: el mensaje "Jimmy, tu cambio de inversión se realizó con éxito" debe ser reemplazado por "Usuario, tu cambio de inversión se realizó con exito." Ejemplo2: el mensaje "El monto ingresado debe ser mayor a S/1700", debe ser remplazado por "El monto ingresado debe ser mayor a N "
+event: "prestamype.modales",
+modal_mensajePrincipal: "Tú cámara no está activa", // Mensaje principal o título del modal.  En caso incluya algún dato del usuario, como su nombre, debe ser reemplazado por un texto genérico. Ejemplo1: el mensaje "Jimmy, tu cambio de inversión se realizó con éxito" debe ser reemplazado por "Usuario, tu cambio de inversión se realizó con exito." Ejemplo2: el mensaje "El monto ingresado debe ser mayor a S/1700", debe ser remplazado por "El monto ingresado debe ser mayor a N "
+modal_mensajeSecundario: "Por favor recarga la página, y acepta los permisos de tu cámara. ", //En caso esté disponible. También debe reemplzarse datos del relacionados al usuario (nombres,montos) por textos genericos.
+//PRODUCTO: Ensaso esten dispibles
+producto: "inversion", // Producto en cuyo plujo de mostró el modal
+producto_tipo: "factoring", // En caso esté disponible
+//INSTITUCIONAL o Persona Natural: En caso esté disponible
+operacion_perfilTipo: "Institucional", //Persona Natural o Institucional
 });
 
