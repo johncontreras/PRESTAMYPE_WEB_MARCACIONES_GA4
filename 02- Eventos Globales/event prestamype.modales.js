7e1ec6113e3c44572f1686ccc874d213fa0148c9
John Alex Contreras Soto <johnalex.cs@gmail.com>
2022-10-09 19:40:36 -0500

    Update 02- Eventos Globales/event prestamype.modales.js

--- 02- Eventos Globales/event prestamype.modales.js
+++ 02- Eventos Globales/event prestamype.modales.js
@@ -7,6 +7,7 @@
 window.dataLayer = window.dataLayer || [];
 window.dataLayer.push({
   event: "prestamype.modales",
-  modal_tipo: "/solicitud/datos_personales_1", //Path o ruta de la URL (sin hostname/dominio)
-  modal_mensajeUsuario: "Préstamos con garantía hipotecaria y Factoring", //Título del documento actual.
+  modal_categoria: "Datos Faltantes", //posibles valores: Informativo, Datos Faltantes, Datos Incorrectos, Alerta
+  modal_mensajeUsuario: "Existe un registro que falta completar", // Mensaje principal del modal. En caso incluya algún dato del usuario, como su nombre, debe ser reemplazado por un texto genérico. Ejemplo1: el mensaje "Jimmy, tu cambio de inversión se realizó con éxito" debe ser reemplazado por "Usuario, tu cambio de inversión se realizó con exito." Ejemplo2: el mensaje "El monto ingresado debe ser mayor a S/1700", debe ser remplazado por "El monto ingresado debe ser mayor a N "
 });
+
