9cfcf8efee9fe7280caf5af1a085311f8b974582
John Alex Contreras Soto <johnalex.cs@gmail.com>
2022-10-09 19:45:55 -0500

    Update 02- Eventos Globales/event prestamype.modales.js

--- 02- Eventos Globales/event prestamype.modales.js
+++ 02- Eventos Globales/event prestamype.modales.js
@@ -8,6 +8,7 @@ window.dataLayer = window.dataLayer || [];
 window.dataLayer.push({
   event: "prestamype.modales",
   modal_categoria: "Datos Faltantes", //posibles valores: Informativo, Datos Faltantes, Datos Incorrectos, Alerta
-  modal_mensajeUsuario: "Existe un registro que falta completar", // Mensaje principal del modal. En caso incluya algún dato del usuario, como su nombre, debe ser reemplazado por un texto genérico. Ejemplo1: el mensaje "Jimmy, tu cambio de inversión se realizó con éxito" debe ser reemplazado por "Usuario, tu cambio de inversión se realizó con exito." Ejemplo2: el mensaje "El monto ingresado debe ser mayor a S/1700", debe ser remplazado por "El monto ingresado debe ser mayor a N "
+
+  modal_mensajeUsuario: "Tú cámara no está activa" // Mensaje principal del modal.  En caso incluya algún dato del usuario, como su nombre, debe ser reemplazado por un texto genérico. Ejemplo1: el mensaje "Jimmy, tu cambio de inversión se realizó con éxito" debe ser reemplazado por "Usuario, tu cambio de inversión se realizó con exito." Ejemplo2: el mensaje "El monto ingresado debe ser mayor a S/1700", debe ser remplazado por "El monto ingresado debe ser mayor a N "
 });
 
