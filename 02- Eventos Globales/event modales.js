3b15b40fbda83ee4b38be21dc8aa5f1fec0244de
John Alex Contreras Soto <johnalex.cs@gmail.com>
2022-10-09 19:20:08 -0500

    Update 02- Eventos Globales/event modales.js

--- 02- Eventos Globales/event modales.js
+++ 02- Eventos Globales/event modales.js
@@ -1,8 +1,9 @@
-//Disparador: Cada vez que se cambie de vista (cada vez que haya un cambio en la URL).
+//Disparador: Cada vez que se muestre un modal con un mensaje para el usuario.
+//Ruta: Zona Privda /app/
 
 window.dataLayer = window.dataLayer || [];
 window.dataLayer.push({
-  event: "virtualPageView",
-  virtualPagePath: "/solicitud/datos_personales_1", //Path o ruta de la URL (sin hostname/dominio)
-  virtualPageTitle: "Préstamos con garantía hipotecaria y Factoring", //Título del documento actual.
+  event: "prestamype.modales",
+  modal_tipo: "/solicitud/datos_personales_1", //Path o ruta de la URL (sin hostname/dominio)
+  modal_mensajeUsuario: "Préstamos con garantía hipotecaria y Factoring", //Título del documento actual.
 });
