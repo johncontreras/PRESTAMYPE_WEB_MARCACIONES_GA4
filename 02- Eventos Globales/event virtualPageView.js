f2dcda416adfcb2a6d9a41129e3c8d4acd867867
John Alex Contreras Soto <johnalex.cs@gmail.com>
2022-10-10 21:03:07 -0500

    Update 02- Eventos Globales/event virtualPageView.js

--- 02- Eventos Globales/event virtualPageView.js
+++ 02- Eventos Globales/event virtualPageView.js
@@ -5,4 +5,6 @@ window.dataLayer.push({
   event: "virtualPageView",
   virtualPagePath: "/solicitud/datos_personales_1", //Path o ruta de la URL (sin hostname/dominio)
   virtualPageTitle: "Préstamos con garantía hipotecaria y Factoring", //Título del documento actual.
+  lead_id:"36c5e31e2a3db3189a4e0f1dc3b21500b153df911f831384730791f5d0b3d7ca",
+  user_id:"a8c64670fc3bddc258b7f1e01bbfcf1a5823daaee58b5175c4b3f6c000af5fa3",
 });
