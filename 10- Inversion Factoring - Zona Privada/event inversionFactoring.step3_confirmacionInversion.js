26034d81b493ed372d63713be6f4a264d9b6604d
John Alex Contreras Soto <johnalex.cs@gmail.com>
2022-10-12 13:08:50 -0500

    Update 10- Inversion Factoring - Zona Privada/event inversionFactoring.step3_confirmacionInversion.js

--- 10- Inversion Factoring - Zona Privada/event inversionFactoring.step3_confirmacionInversion.js
+++ 10- Inversion Factoring - Zona Privada/event inversionFactoring.step3_confirmacionInversion.js
@@ -27,6 +27,8 @@ window.dataLayer.push({
   cliente_nombre: "CONSTRUCTORA INARCO PERU S.A.C.", //Nomre del cliente (organizacion que vende Factura)
   subasta_codigo: "l7yz6pqt",
   subasta_modelo: "velocidad", // String: Modelo seleccionado de la subasta.
+  subasta_descuento: "Desde 11% hasta 12%",
+
   subasta_moneda: "PEN",
   riesgo: "C", //String. Riesgo asignado a la subasta.
   rendimiento: "1.35", //String. Rendimiento/TIR. Máximo dos decimales.(1.35% = 1.35)
