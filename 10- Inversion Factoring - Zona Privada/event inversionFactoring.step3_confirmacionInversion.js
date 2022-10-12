7453da2f246d831bc4ef62babd2a92326e36376e
John Alex Contreras Soto <johnalex.cs@gmail.com>
2022-10-12 12:57:20 -0500

    Update 10- Inversion Factoring - Zona Privada/event inversionFactoring.step3_confirmacionInversion.js

--- 10- Inversion Factoring - Zona Privada/event inversionFactoring.step3_confirmacionInversion.js
+++ 10- Inversion Factoring - Zona Privada/event inversionFactoring.step3_confirmacionInversion.js
@@ -19,11 +19,12 @@ window.dataLayer.push({
 
   //INSTITUCIONAL o Persona Natural
   operacion_perfilTipo: "Institucional", //Persona Natural o Institucional
+  operacion_perfilCategoria: "VIP",
   operacion_perfilRuc: "20984676233", //RUC. solo en caso de perfil Institucional
   operacion_perfilEmpresa: "TALENTO S.A.", //Razon Social de la empresa. solo en caso de perfil Institucional
 
   //Oportunidad:
-  empresa_nombre: "CONSTRUCTORA INARCO PERU S.A.C.", //Nomre del cliente (organizacion que vende Factura)
+  cliente_nombre: "CONSTRUCTORA INARCO PERU S.A.C.", //Nomre del cliente (organizacion que vende Factura)
   subasta_codigo: "l7yz6pqt",
   subasta_modelo: "velocidad", // String: Modelo seleccionado de la subasta.
   subasta_moneda: "PEN",
