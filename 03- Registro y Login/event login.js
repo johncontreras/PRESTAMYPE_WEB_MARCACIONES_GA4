cc5eb668d93e6c705966ead53e40b302a1172925
John Alex Contreras Soto <johnalex.cs@gmail.com>
2022-12-20 12:44:28 -0500

    Update 03- Registro y Login/event login.js

--- 03- Registro y Login/event login.js
+++ 03- Registro y Login/event login.js
@@ -14,5 +14,8 @@ window.dataLayer.push({
   user_email: "johnalex.cs@gmail.com", //email ingresado en el formulario.
   user_id: "628c18cc9de1a9674ab8fb20", // En caso este disponible. Id único generado a partir de su DNI.
   user_tipo: "registrado", //registrado: para usuarios registrados en plataforma. "no registrado": para usuarios no registrados.
-  visita_tipo: "logueado" //logueado: para usuarios son sesion inicida en plataforma. "no logueado": para usuarios sin sesion iniciada.
+  visita_tipo: "logueado", //logueado: para usuarios son sesion inicida en plataforma. "no logueado": para usuarios sin sesion iniciada.
+  user_inversiones: 2, //Número entero. Número de inversiones previas realizadas.
+  user_subastas:0,//Número entero. Número de subastas previas realizadas.
+  user_recurrente: true, //Booleano: Si tiene  operaciones (inversiones o subastas) previas: true, si no tiene operaciones previas: false
 });
